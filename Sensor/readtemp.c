f1d9166366729e682073cec7dc5dda35eafef5c3
Michael Rayhelson <mray772@gmail.com>
2014-09-09 06:00:13 +0000

    checkpoint

--- Sensor/readtemp.c
+++ Sensor/readtemp.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define PIN 18
+#define NUMPINS 32
 
 typedef enum {
     START = 0,
@@ -15,78 +15,91 @@ typedef enum {
     BIT = 6
 } State;
 
-State state = START;
-int bitNumber =0;
-unsigned char data[5];
 
-int z = 0;
-int last =0;
+struct PinData {
+     State state;
+     int bitNumber;
+     unsigned char data[5]; 
+     int lastTick; 
+};
+
+struct PinData Pins[NUMPINS];
+char activePins[NUMPINS];
+int nActive = 0;
+
+//////////////////////////////////////////
+
+void resetPinData(struct PinData* pd) {
+    pd->state = START;
+    pd->bitNumber = 0;
+    pd->lastTick = 0;
+    memset(pd->data, 0, 5);
+}
+//////////////////////////////////////////
+
+void parseArgs() {
+     activePins[0] = 4;
+     activePins[1] = 18;
+     nActive = 2;
+}
+
+
 void gpioAlertFunc (int gpio, int level, uint32_t tick) {
-   //printf("SIG %i %i %u\n", level, state,  tick - last);
-   if (state == START) {
-       bitNumber = 0;
-       memset(data,0,5);
-       //printf("\n");
-   }   
-   if (state == BIT) {
-//       printf("%i", tick - last > 50 ? 1:0); 
-       if (tick - last > 50) {
-           data[bitNumber / 8] |= (0x80 >> (bitNumber % 8)); 
+   if (Pins[gpio].state == BIT) {
+       if (tick - Pins[gpio].lastTick > 50) {
+           int bit = Pins[gpio].bitNumber;
+           Pins[gpio].data[bit / 8] |= (0x80 >> (bit % 8)); 
        }
-       bitNumber++;
-       if(bitNumber == 40) {
-///           printf("\n");
-///           printf("%02x%02x%02x%02x%02x,", data[0], data[1], data[2], data[3], data[4]);
-           unsigned char chksum = data[0];
+       Pins[gpio].bitNumber++;
+       if(Pins[gpio].bitNumber == 40) {
+           unsigned char chksum = Pins[gpio].data[0];
            int c;
            for (c =1; c <4; c++) {
-               chksum += data[c];
+               chksum += Pins[gpio].data[c];
            }
-           printf("%d.%d,%d.%d,%s\n", data[0], data[1], data[2], data[3], chksum == data[4] ? "OK" : "CRP");
+           printf("%d.%d,%d.%d,%s\n", Pins[gpio].data[0], Pins[gpio].data[1], Pins[gpio].data[2], Pins[gpio].data[3], chksum == Pins[gpio].data[4] ? "OK" : "CRP");
        }
    }
 
-   switch(state) {
-       case START: state = DOWN18ms; break;
-       case DOWN18ms: state = UP40us; break;
-       case UP40us: state = DOWN80us; break;
-       case DOWN80us: state = UP80us; break;
-       case UP80us: state = PREFIX; break;
-       case PREFIX: state = BIT; break;
-       case BIT: state = PREFIX; break;
+   switch(Pins[gpio].state) {
+       case START: Pins[gpio].state = DOWN18ms; break;
+       case DOWN18ms: Pins[gpio].state = UP40us; break;
+       case UP40us: Pins[gpio].state = DOWN80us; break;
+       case DOWN80us: Pins[gpio].state = UP80us; break;
+       case UP80us: Pins[gpio].state = PREFIX; break;
+       case PREFIX: Pins[gpio].state = BIT; break;
+       case BIT: Pins[gpio].state = PREFIX; break;
    } 
-   last = tick;
-   z++;
+   Pins[gpio].lastTick = tick;
 }
 
 
 void main() {
+   int i;
    int ret = gpioCfgClock(10, 1, 1);
    if (gpioInitialise() < 0) {
      printf("Failed to initialize PGPIO\n");
    }
-   ret = gpioSetAlertFunc(PIN, gpioAlertFunc);
+   parseArgs();
 
-   //printf("ret = %i\n", ret);
-   int i;
-   //for (i =0; i<30; i++) {
-   {
-   state = START;
-   ret = gpioSetMode(PIN, PI_INPUT);
-   sleep(1);
-//   ret = gpioSetMode(PIN, PI_OUTPUT);
-   ret = gpioWrite(PIN, PI_LOW);
-   ///printf("%u, OK\n", gpioTick());
-   usleep(10000);
-   ret = gpioWrite(PIN, PI_HIGH);
-   //printf("%u, OK\n", gpioTick());
-   ret = gpioSetMode(PIN, PI_INPUT);
-   
-   ret= gpioTick();
-   int ret2= gpioTick();
-   ///gpioSleep(PI_TIME_RELATIVE, 10 ,0);
-   usleep(10000);
-   ///printf("%u %u %i, --\n", ret, ret2, z);
-   z=0;
+   for (i = 0; i < nActive; i++) {
+        resetPinData(&Pins[activePins[i]]);
+        gpioSetAlertFunc(activePins[i], gpioAlertFunc);
+        gpioSetMode(activePins[i], PI_INPUT);
    }
+
+   usleep(50000);
+
+   for (i = 0; i < nActive; i++) {
+       ret = gpioWrite(activePins[i], PI_LOW);
+   }
+
+   usleep(18000);
+   
+   for (i = 0; i < nActive; i++) {
+       ret = gpioWrite(activePins[i], PI_HIGH);
+       ret = gpioSetMode(activePins[i], PI_INPUT);
+   }
+   
+   usleep(100000);
 }
